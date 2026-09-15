b130acb08c5b848529f97e938e598fd151ae2b17
wcs7777 <wcs777git@gmail.com>
2026-09-15 01:20:35 -0300

    update post-dotbot.sh

--- setup-scripts/post-dotbot.sh
+++ setup-scripts/post-dotbot.sh
@@ -3,6 +3,7 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${BASEDIR}/../install-scripts/nix-home-manager.sh"
+sudo systemctl reset-failed
 zsh -c "home-manager switch"
 
 [[ $SHELL != /bin/zsh ]] && chsh -s /bin/zsh
