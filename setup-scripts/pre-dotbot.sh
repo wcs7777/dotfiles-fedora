4248cf10cfe810e219d1e4ce2002cb303d29059d
wcs7777 <wcs777git@gmail.com>
2026-09-15 00:32:23 -0300

    simplify setup-scripts/pre-dotbot.sh moving some commands to install.config

--- setup-scripts/pre-dotbot.sh
+++ setup-scripts/pre-dotbot.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 sudo dnf upgrade --assumeyes
-
-sudo dnf install dbus  --assumeyes
-sudo systemctl start dbus
-sudo systemctl reset-failed
-
-sudo dnf install git python --assumeyes
-
-source "${BASEDIR}/config-locales.sh"
+sudo dnf install dbus git python --assumeyes
