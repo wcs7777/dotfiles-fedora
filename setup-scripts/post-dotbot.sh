cd61afacf2152387c5716351dc910139a39b6d3a
wcs7777 <wcs777git@gmail.com>
2026-09-15 01:11:01 -0300

    add setup-scripts/post-dotbot.sh

---
+++ setup-scripts/post-dotbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${BASEDIR}/install-nix-home-manager.sh"
+zsh -c "home-manager switch"
+
+[[ $SHELL != /bin/zsh ]] && chsh -s /bin/zsh
