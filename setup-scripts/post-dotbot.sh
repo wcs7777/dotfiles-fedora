65fdc4b6652ba90b35f9d454a5c6a946d1f68404
wcs7777 <wcs777git@gmail.com>
2026-09-15 00:01:52 -0300

    remove unused scripts

--- setup-scripts/post-dotbot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source "${BASEDIR}/../install-scripts/nix-home-manager.sh"
-chsh -s /bin/zsh
-zsh -c "home-manager switch"
-zsh -c "mise install"
